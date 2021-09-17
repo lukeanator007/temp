bd707d03c292ed893b36fd937fc702582be1ed91
lukeanator007 <39265269+lukeanator007@users.noreply.github.com>
2021-09-17 21:11:39 +0100

    Add files via upload

---
+++ TestVantage/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TestVantage.Services;
+using TestVantage.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestVantage
+	{
+	public partial class App : Application
+		{
+		public static long TimeoutLength { get; private set; }
+		public static long LastActive;
+		public static Boolean LoggedIn;
+
+		public App()
+			{
+			InitializeComponent();
+
+			TimeoutLength = 2000000000;
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new AppShell();
+			}
+
+		protected override void OnStart()
+			{
+			}
+
+		protected override void OnSleep()
+			{
+			if (LoggedIn)
+				LastActive = DateTime.UtcNow.Ticks;
+			}
+
+		protected async override void OnResume()
+			{
+			var Ticks = DateTime.UtcNow.Ticks - LastActive;
+			if ( Ticks > 0 && Ticks < TimeoutLength)
+				{
+				LoggedIn = true;
+				await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+				}
+			else
+				{
+				LoggedIn = false;
+				await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+				}
+
+			}
+		}
+	}
---
+++ TestVantage/Models/PictureItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TestVantage.Models
+	{
+	public class PictureItem
+		{
+		public ImageSource Source { get; set; }
+
+		public PictureItem(String uri) 
+			{
+			Source = ImageSource.FromUri(new Uri(uri));
+			}
+		}
+	}
